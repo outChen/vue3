db5ea61d8c6e1676617381426956e263225dfc7a
v_cxiqchen(陈细庆) <v_cxiqchen@tencent.com>
2022-05-30 19:17:04 +0800

    处理找不到app.vue报错

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/App.vue'
+import HomeView from '@/views/componets/Name/index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
